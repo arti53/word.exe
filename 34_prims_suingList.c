768c8f34400674fd4799ee9a99fde2121da801f7
Astha Chaudhari <143062179+astha213@users.noreply.github.com>
2024-05-27 21:26:55 +0530

    Update 34_prims_suingList.c

--- 34_prims_suingList.c
+++ 34_prims_suingList.c
@@ -1,133 +1,107 @@
 #include <stdio.h>
-#include<stdlib.h>
-
-#define MAX_VERTICES 100 
+#include <stdlib.h>
 
 struct node {
-    int vertex;
-    int weight;
+    int vertex, weight;
     struct node *next;
 };
 
-struct node *A[MAX_VERTICES];
-
-// Using List starts here  :  
-int mindist(int a[], int b[], int n) {
-    int min = 999, ind = -1;
-
+void createAdjList(struct node *A[], int n) {
+    int edges;
     for (int i = 0; i < n; i++) {
-        if (!b[i] && a[i] < min) {
-            min = a[i];
-            ind = i;
-        }
-    }
-    return ind;
-}
-
-void display(int parent[], int n) {
-    printf("\n\nMinimum spanning tree is : ");
-    for (int i = 1; i < n; i++) {
-        printf("%d - %d \n", parent[i], i);
-    }
-}
-
-void prims(int n) {
-    int visited[n];
-    int dist[n], parent[n], u;  
-
-    for (int i = 0; i < n; i++) {
-        visited[i] = 0; 
-        parent[i] = -1;
-        dist[i] = 999;
-    }
-
-    dist[0] = 0;
-    for (int i = 0; i < n - 1; i++) {
-        u = mindist(dist, visited, n); 
-        visited[u] = 1;
-
-        struct node *temp = A[u];
-
-        while (temp != NULL) {
-            int v = temp->vertex;
-            int weight = temp->weight;
-            if (!visited[v] && weight < dist[v]) {
-                dist[v] = weight;
-                parent[v] = u;
-            }
-            temp = temp->next;
-        }
-    }
-    display(parent, n); 
-}
-
-int list(){
-    int u, v, w, n;
-    char ch;
-    printf("Enter the number of vertices : ");
-    scanf("%d", &n);
-
-    for (int i = 0; i < n; i++){
         A[i] = NULL;
     }
+    printf("Enter no. of edges : ");
+    scanf("%d", &edges);
+    for (int i = 0; i < edges; i++) {
+        int startvertex, endvertex, weight;
+        printf("Enter startvertex, endvertex, weight : ");
+        scanf("%d %d %d", &startvertex, &endvertex, &weight);
 
-    struct node *new;
-    do{
-        printf("\nEnter edge : ");
-        scanf("%d %d", &u, &v);
-        printf("\nEnter weight from edge %d to %d :  ", u, v);
-        scanf("%d", &w);
+        // Create new node for endvertex in adjacency list of startvertex
+        struct node *newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->vertex = endvertex;
+        newnode->weight = weight;
+        newnode->next = A[startvertex];
+        A[startvertex] = newnode;
 
-        new = (struct node *)malloc(sizeof(struct node));
-        new->vertex = v;
-        new->weight = w;
-        new->next = A[u];
-        A[u] = new;
+        // Create new node for startvertex in adjacency list of endvertex
+        newnode = (struct node *)malloc(sizeof(struct node));
+        newnode->vertex = startvertex;
+        newnode->weight = weight;
+        newnode->next = A[endvertex];
+        A[endvertex] = newnode;
+    }
+}
 
-        new = (struct node *)malloc(sizeof(struct node));
-        new->vertex = u;
-        new->weight = w;
-        new->next = A[v];
-        A[v] = new;
-
-        printf("\nDo you want to enter more edges (y/n) : ");
-        scanf(" %c", &ch);
-    } while (ch == 'y' || ch == 'Y');
-
-    printf("\nAdjacency List:\n");
+void displayAdjList(struct node *A[], int n) {
+    printf("Adjacency List:\n");
     for (int i = 0; i < n; i++) {
-        struct node *p = A[i];
-        printf("%d: ", i);
+        printf("Vertex %d: ", i);
+        struct node *current = A[i];
+        while (current != NULL) {
+            printf("(%d, %d) ", current->vertex, current->weight);
+            current = current->next;
+        }
+        printf("\n");
+    }
+}
+
+void display(int dist[], int par[], int n) {
+    printf("Edge Weight\n");
+    for (int i = 1; i < n; i++) {
+        printf("%d - %d %d \n", par[i], i, dist[i]);
+    }
+}
+
+int minimum(int dist[], int visited[], int n) {
+    int min = 99, index;
+    for (int i = 0; i < n; i++) {
+        if (dist[i] < min && visited[i] == 0) {
+            min = dist[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+void adjListPrims(struct node *A[], int n) {
+    int visited[n], dist[n], par[n], v, u;
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+        dist[i] = 99;
+        par[i] = -1;
+    }
+    printf("Enter start vertex : ");
+    scanf("%d", &v);
+    par[v] = v;
+    dist[v] = 0;
+    for (int i = 0; i < n - 1; i++) {
+        u = minimum(dist, visited, n);
+        visited[u] = 1;
+        struct node *p = A[u];
         while (p != NULL) {
-            printf("(%d, %d) -> ", p->vertex, p->weight);
+            if (visited[p->vertex] == 0 && p->weight < dist[p->vertex]) {
+                par[p->vertex] = u;
+                dist[p->vertex] = p->weight;
+            }
             p = p->next;
         }
-        printf("NULL\n");
     }
-
-    return n;
+    display(dist, par, n);
 }
 
-int main() { 
-    int m,f;
-    while(1){
-        printf("\n1.Prims using Adjacency List \n2.Exit \n");
-        printf("\nEnter which algorithm : ");
-        scanf("%d",&m);
-        switch(m){
-            case 1:
-                f = list();
-                prims(f);
-                break;
-            case 2:
-                exit(0);
-            default:
-                printf("\nEnter a valid option.");
-        }
-    }
-
+int main() {
+    int n;
+    printf("Enter number of vertices : ");
+    scanf("%d", &n);
+    struct node *A[n];
+    createAdjList(A, n);
+    displayAdjList(A, n);
+    adjListPrims(A, n);
     return 0;
 }
+
 
 // 6
 // 9
