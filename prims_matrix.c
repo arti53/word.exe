513dcc008e090d750d6b9662a1ca442816d53f86
sonali-53 <157357095+sonali-53@users.noreply.github.com>
2024-05-27 08:27:08 +0530

    Create prims_matrix.c

---
+++ prims_matrix.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+#define MAX_VERTICES 100 
+int G[MAX_VERTICES][MAX_VERTICES];
+
+//Using matrix : 
+int mindistmatrix(int a[], int b[], int n) {
+    int min = 999, ind = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (!b[i] && a[i] < min) {
+            min = a[i];
+            ind = i;
+        }
+    }
+    return ind;
+}
+
+void displaymatrix(int parent[], int G[][MAX_VERTICES], int n) {
+    printf("\n\nMinimum spanning tree is : ");
+    for (int i = 1; i < n; i++) {
+        printf("%d - %d \n", parent[i], i);
+    }
+}
+
+void primsmatrix(int G[][MAX_VERTICES], int n) {
+    int visited[n];
+    int dist[n], parent[n], u;  
+
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0; 
+        parent[i] = -1;
+        dist[i] = 999;
+    }
+
+    dist[0] = 0;
+    for (int i = 0; i < n - 1; i++) {
+        u = mindistmatrix(dist, visited, n); 
+        visited[u] = 1;
+
+        for (int j = 0; j < n; j++) {
+            if (G[u][j] != 999 && G[u][j] < dist[j] && !visited[j]) {
+                dist[j] = G[u][j];
+                parent[j] = u;
+            }
+        }
+    }
+    displaymatrix(parent, G, n); 
+}
+
+int matrix(){
+    int v, e, a, b, w;
+    printf("\nEnter the number of vertices: ");
+    scanf("%d", &v);
+
+    for (int i = 0; i < v; i++) {
+        for (int j = 0; j < v; j++) {
+            G[i][j] = 999;
+        }
+    }
+
+    printf("\nEnter the number of edges: ");
+    scanf("%d", &e);
+
+    for (int i = 0; i < e; i++) {
+        printf("\nEnter edge: ");
+        scanf("%d %d", &a, &b);
+        printf("Enter weight from edge %d to %d: ", a, b);
+        scanf("%d", &w);
+        G[a][b] = G[b][a] = w;
+    }
+
+    printf("\nAdjacency matrix:\n");
+    for (int i = 0; i < v; i++) {
+        for (int j = 0; j < v; j++) {
+            printf("%5d ", G[i][j]);
+        }
+        printf("\n");
+    }
+
+    return v;
+}
+
+int main() { 
+    int m,f;
+    while(1){
+        printf("\n1.Prims Algorithm using matrix \n2.Exit \n");
+        printf("\nEnter which algorithm : ");
+        scanf("%d",&m);
+        switch(m){
+            case 1:
+                f = matrix();
+                primsmatrix(G,f);
+                break;
+            case 2:
+                exit(0);
+            default:
+                printf("\nEnter a valid option.");
+        }
+    }
+
+    return 0;
+}
