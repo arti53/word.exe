03a2e00da1dd704aa15bfd066023a96d58de7ddc
sonali-53 <157357095+sonali-53@users.noreply.github.com>
2024-05-27 08:50:12 +0530

    Update 10_mergedLL.c

--- 10_mergedLL.c
+++ 10_mergedLL.c
@@ -18,7 +18,7 @@ struct Node* createNode(int data) {
 }
 
 // Function to insert a node at the end
-void insertAtEnd(struct Node** head, int data) {
+void insert(struct Node** head, int data) {
     struct Node* newNode = createNode(data);
     if (*head == NULL) {
         *head = newNode;
@@ -43,41 +43,45 @@ void printList(struct Node* node) {
 
 // Function to merge two sorted doubly linked lists
 struct Node* mergeLists(struct Node* head1, struct Node* head2) {
+    struct Node* t1 = head1;
+    struct Node* t2 = head2;
     struct Node* mergedHead = NULL;
     struct Node* temp;
 
-    if (head1 == NULL) return head2;
-    if (head2 == NULL) return head1;
+    if (t1 == NULL) 
+        return t2;
+    if (t2 == NULL) 
+        return t1;
 
-    if (head1->data <= head2->data) {
-        mergedHead = head1;
-        head1 = head1->next;
+    if (t1->data <= t2->data) {
+        mergedHead = t1;
+        t1 = t1->next;
     } else {
-        mergedHead = head2;
-        head2 = head2->next;
+        mergedHead = t2;
+        t2 = t2->next;
     }
 
     temp = mergedHead;
 
-    while (head1 != NULL && head2 != NULL) {
-        if (head1->data <= head2->data) {
-            temp->next = head1;
-            head1->prev = temp;
-            head1 = head1->next;
+    while (t1 != NULL && t2 != NULL) {
+        if (t1->data <= t2->data) {
+            temp->next = t1;
+            t1->prev = temp;
+            t1 = t1->next;
         } else {
-            temp->next = head2;
-            head2->prev = temp;
-            head2 = head2->next;
+            temp->next = t2;
+            t2->prev = temp;
+            t2 = t2->next;
         }
         temp = temp->next;
     }
 
-    if (head1 != NULL) {
-        temp->next = head1;
-        head1->prev = temp;
-    } else if (head2 != NULL) {
-        temp->next = head2;
-        head2->prev = temp;
+    if (t1 != NULL) {
+        temp->next = t1;
+        t1->prev = temp;
+    } else if (t2 != NULL) {
+        temp->next = t2;
+        t2->prev = temp;
     }
 
     return mergedHead;
@@ -87,30 +91,58 @@ int main() {
     struct Node* head1 = NULL;
     struct Node* head2 = NULL;
     struct Node* mergedHead = NULL;
+    int choice, data;
 
-    // Create first sorted doubly linked list: 1 <-> 3 <-> 5 <-> 7
-    insertAtEnd(&head1, 1);
-    insertAtEnd(&head1, 2);
-    insertAtEnd(&head1, 5);
-    insertAtEnd(&head1, 7);
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1. Insert a node into List 1\n");
+        printf("2. Insert a node into List 2\n");
+        printf("3. Print List 1\n");
+        printf("4. Print List 2\n");
+        printf("5. Merge the two lists\n");
+        printf("6. Print Merged List\n");
+        printf("7. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    // Create second sorted doubly linked list: 2 <-> 4 <-> 6 <-> 8
-    insertAtEnd(&head2, 2);
-    insertAtEnd(&head2, 4);
-    insertAtEnd(&head2, 6);
-    insertAtEnd(&head2, 8);
-
-    printf("List 1: ");
-    printList(head1);
-
-    printf("List 2: ");
-    printList(head2);
-
-    // Merge the two sorted lists
-    mergedHead = mergeLists(head1, head2);
-
-    printf("Merged List: ");
-    printList(mergedHead);
+        switch (choice) {
+            case 1:
+                printf("Enter the data to insert into List 1: ");
+                scanf("%d", &data);
+                insert(&head1, data);
+                break;
+            case 2:
+                printf("Enter the data to insert into List 2: ");
+                scanf("%d", &data);
+                insert(&head2, data);
+                break;
+            case 3:
+                printf("List 1: ");
+                printList(head1);
+                break;
+            case 4:
+                printf("List 2: ");
+                printList(head2);
+                break;
+            case 5:
+                mergedHead = mergeLists(head1, head2);
+                printf("Lists merged successfully.\n");
+                break;
+            case 6:
+                if (mergedHead != NULL) {
+                    printf("Merged List: ");
+                    printList(mergedHead);
+                } else {
+                    printf("Lists have not been merged yet.\n");
+                }
+                break;
+            case 7:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
 
     return 0;
 }
