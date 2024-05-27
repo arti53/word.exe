1f39971337af039d9a928014a9afe534fd32c17f
Parth-Kedari <157357095+Parth-Kedari@users.noreply.github.com>
2024-05-27 08:12:55 +0530

    Update 11_Push_pop_stack_doubleLL.c

--- 11_Push_pop_stack_doubleLL.c
+++ 11_Push_pop_stack_doubleLL.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Structure for doubly linked list node
 struct Node {
     int data;
     struct Node* prev;
     struct Node* next;
-} Node;
+};
 
-// Definition of the stack using a doubly linked list
+// Structure for stack
 typedef struct Stack {
-    struct Node* top; 
+    struct Node* top;
 } Stack;
 
+// Function prototypes
+struct Node* createNode(int data);
+void initializeStack(Stack* stack);
+void push(Stack* stack, int data);
+int pop(Stack* stack);
+void displayStack(Stack* stack);
+
+// Function to create a new doubly linked list node
 struct Node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(Node));
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     if (!newNode) {
         printf("Memory allocation error\n");
         return NULL;
@@ -24,10 +33,12 @@ struct Node* createNode(int data) {
     return newNode;
 }
 
+// Function to initialize the stack
 void initializeStack(Stack* stack) {
     stack->top = NULL;
 }
 
+// Function to push an element onto the stack
 void push(Stack* stack, int data) {
     struct Node* newNode = createNode(data);
     if (!newNode) {
@@ -43,6 +54,7 @@ void push(Stack* stack, int data) {
     printf("%d pushed onto stack\n", data);
 }
 
+// Function to pop an element from the stack
 int pop(Stack* stack) {
     if (stack->top == NULL) {
         printf("Stack Underflow\n");
@@ -59,6 +71,7 @@ int pop(Stack* stack) {
     return poppedData;
 }
 
+// Function to display the elements of the stack
 void displayStack(Stack* stack) {
     struct Node* current = stack->top;
     if (current == NULL) {
@@ -76,22 +89,36 @@ void displayStack(Stack* stack) {
 int main() {
     Stack stack;
     initializeStack(&stack);
+    int choice, data;
 
-    push(&stack, 10);
-    push(&stack, 20);
-    push(&stack, 30);
-    displayStack(&stack);
+    while (1) {
+        printf("\nStack Operations:\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Display Stack\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    pop(&stack);
-    displayStack(&stack);
-
-    pop(&stack);
-    displayStack(&stack);
-
-    pop(&stack);
-    displayStack(&stack);
-
-    pop(&stack);
+        switch (choice) {
+            case 1:
+                printf("Enter data to push: ");
+                scanf("%d", &data);
+                push(&stack, data);
+                break;
+            case 2:
+                pop(&stack);
+                break;
+            case 3:
+                displayStack(&stack);
+                break;
+            case 4:
+                printf("Exiting program\n");
+                exit(0);
+            default:
+                printf("Invalid choice\n");
+        }
+    }
 
     return 0;
 }
