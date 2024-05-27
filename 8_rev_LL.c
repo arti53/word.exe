f7f434d9bdc6ccfb2f621d491dbd46f93f971bd1
Parth-Kedari <157357095+Parth-Kedari@users.noreply.github.com>
2024-05-27 07:32:40 +0530

    Update 8_rev_LL.c

--- 8_rev_LL.c
+++ 8_rev_LL.c
@@ -1,64 +1,171 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node{
+// Define the structure for a doubly linked list node
+struct Node {
     int data;
     struct Node* next;
+    struct Node* prev;
 };
 
-struct Node* newNode(int data)
-{
-    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
-    node -> data = data;
-    node -> next = NULL;
-    return node;
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
 }
 
-void print(struct Node* head)
-{
-    while(head != NULL)
-    {
-        printf("%d ",head->data);
-        head = head -> next;
+// Function to insert a node at the beginning
+void insertAtBeginning(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head != NULL) {
+        newNode->next = *head;
+        (*head)->prev = newNode;
     }
+    *head = newNode;
+}
 
+// Function to insert a node at the end
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->prev = temp;
+}
+
+// Function to insert a node after a given node
+void insertAfter(struct Node* prevNode, int data) {
+    if (prevNode == NULL) {
+        printf("Previous node cannot be NULL\n");
+        return;
+    }
+    struct Node* newNode = createNode(data);
+    newNode->next = prevNode->next;
+    prevNode->next = newNode;
+    newNode->prev = prevNode;
+    if (newNode->next != NULL) {
+        newNode->next->prev = newNode;
+    }
+}
+
+// Function to delete the first node
+void deleteFirstNode(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    struct Node* temp = *head;
+    *head = (*head)->next;
+    if (*head != NULL) {
+        (*head)->prev = NULL;
+    }
+    free(temp);
+}
+
+// Function to delete the last node
+void deleteLastNode(struct Node** head) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    struct Node* temp = *head;
+    if (temp->next == NULL) {
+        *head = NULL;
+        free(temp);
+        return;
+    }
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->prev->next = NULL;
+    free(temp);
+}
+
+// Function to delete a node with a given value
+void deleteNode(struct Node** head, int key) {
+    if (*head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp != NULL && temp->data != key) {
+        temp = temp->next;
+    }
+    if (temp == NULL) {
+        printf("Node with value %d not found\n", key);
+        return;
+    }
+    if (temp->prev != NULL) {
+        temp->prev->next = temp->next;
+    } else {
+        *head = temp->next;
+    }
+    if (temp->next != NULL) {
+        temp->next->prev = temp->prev;
+    }
+    free(temp);
+}
+
+// Function to print the list
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
     printf("\n");
 }
 
-void reverse(struct Node** head)
-{
-    struct Node *prev = NULL, *current = *head, *nextNode;
-    while(current != NULL)
-    {
-        nextNode = current->next;
-        current->next = prev;
-        prev = current;
-        current = nextNode;
+int main() {
+    struct Node* head = NULL;
+    int choice, data;
+
+    while (1) {
+        printf("\nMenu:\n");
+        printf("1. Insert at beginning\n");
+        printf("2. Insert at end\n");
+        printf("3. Delete the first node\n");
+        printf("4. Delete the last node\n");
+        printf("5. Print the list\n");
+        printf("6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter data to insert at the beginning: ");
+                scanf("%d", &data);
+                insertAtBeginning(&head, data);
+                break;
+            case 2:
+                printf("Enter data to insert at the end: ");
+                scanf("%d", &data);
+                insertAtEnd(&head, data);
+                break;
+            case 3:
+                deleteFirstNode(&head);
+                break;
+            case 4:
+                deleteLastNode(&head);
+                break;
+            case 5:
+                printf("Current list: ");
+                printList(head);
+                break;
+            case 6:
+                printf("Exiting...\n");
+                return 0;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
     }
-    *head = prev;
-}
-
-int main()
-{
-    struct Node* head = newNode(5);
-    head->next = newNode(4);
-    head->next->next = newNode(3);
-    head->next->next->next = newNode(2);
-    head->next->next->next->next = newNode(1);
-
-    printf("Original List: ");
-    print(head);
-
-    reverse(&head);
-
-    printf("Reversed List: ");
-    print(head);
-
-    // while (head != NULL) {
-    //     struct Node* temp = head;
-    //     head = head->next;
-    //     free(temp);
-    // }
-
     return 0;
 }
