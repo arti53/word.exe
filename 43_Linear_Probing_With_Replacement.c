7aee34d618a5245ae3dc360c2eb3d08416c91422
Astha Chaudhari <143062179+astha213@users.noreply.github.com>
2024-05-28 05:58:23 +0530

    Update 43_Linear_Probing_With_Replacement.c

--- 43_Linear_Probing_With_Replacement.c
+++ 43_Linear_Probing_With_Replacement.c
@@ -1,9 +1,7 @@
-// with replacement
-
 #include <stdio.h>
 
-void Insert_HashTable(int arr[],int iKey,int chain[]);
-void Display_HashTable(int arr[],int chain[]);
+void Insert_HashTable(int arr[], int iKey, int chain[]);
+void Display_HashTable(int arr[], int chain[]);
 
 int count = 0;
 
@@ -13,7 +11,7 @@ int main(void)
     int chain[10];
     int iKey;
 
-    for(int i=0;i<10;i++)
+    for (int i = 0; i < 10; i++)
     {
         arr[i] = -1;
         chain[i] = -1;
@@ -21,86 +19,94 @@ int main(void)
 
     printf("Enter Data:\n");
 
-    for(int i=0;i<10;i++)
+    for (int i = 0; i < 10; i++)
     {
-        scanf("%d",&iKey);
-        Insert_HashTable(arr,iKey,chain);
+        scanf("%d", &iKey);
+        Insert_HashTable(arr, iKey, chain);
     }
 
-    Display_HashTable(arr,chain);
-    printf("Count %d",count);
+    Display_HashTable(arr, chain);
+    printf("Count %d\n", count);
 
+    return 0;
 }
 
-void Insert_HashTable(int arr[],int iKey,int chain[])
+void Insert_HashTable(int arr[], int iKey, int chain[])
 {
-    int iPos;
+    int iPos = iKey % 10;
 
-    iPos = iKey%10;
-
-    if(arr[iPos]==-1)
+    if (arr[iPos] == -1)
     {
         arr[iPos] = iKey;
         return;
     }
 
-    if(arr[iPos]!=-1 && arr[iPos]%10!=iPos)
+    if (arr[iPos] != -1 && arr[iPos] % 10 != iPos)
     {
-        for(int i=0;i<10;i++)
+        for (int i = 0; i < 10; i++)
         {
             count++;
-            if(arr[(iPos+i)%10]==-1)
+            int newPos = (iPos + i) % 10;
+            if (arr[newPos] == -1)
             {
-                arr[(iPos+i)%10] = arr[iPos];
+                arr[newPos] = arr[iPos];
                 arr[iPos] = iKey;
-                for(int j=0;j<10;j++)
+                for (int j = 0; j < 10; j++)
                 {
-                    if(chain[j]==iPos)
+                    if (chain[j] == iPos)
                     {
-                        printf("%d\t",iPos);
-                        if(chain[iPos]!=-1)
-                        {
-                           chain[j] = chain[iPos];
-                           chain[iPos] = -1;
-                        }
-                       chain[chain[j]] = (iPos+i)%10;
-                       return;
-                        
+                        chain[j] = newPos;
                     }
                 }
+                chain[iPos] = -1;
+                for (int j = 0; j < 10; j++)
+                {
+                    if (arr[j] != iKey && arr[j] % 10 == iKey % 10 && chain[j] == -1)
+                    {
+                        chain[j] = iPos;
+                        return;
+                    }
+                }
+                return;
             }
         }
     }
 
-    for(int i=0;i<10;i++)
+    for (int i = 0; i < 10; i++)
     {
         count++;
-        if(arr[(iPos+i)%10]==-1)
+        int newPos = (iPos + i) % 10;
+        if (arr[newPos] == -1)
         {
-            arr[(iPos+i)%10] = iKey;
-            for(int j=0;j<10;j++)
+            arr[newPos] = iKey;
+            for (int j = 0; j < 10; j++)
             {
-                if((arr[j]!=iKey) && (arr[j]%10==iKey%10) && (chain[j]==-1))
+                if (arr[j] != iKey && arr[j] % 10 == iKey % 10 && chain[j] == -1)
                 {
-                   chain[j] = (iPos+i)%10;
-                   return;
+                    chain[j] = newPos;
+                    return;
+                }
+                if (arr[j] == iKey && chain[j] == -1)
+                {
+                    chain[j] = newPos;
+                    return;
                 }
             }
+            return;
         }
     }
-
 }
 
-void Display_HashTable(int arr[],int chain[])
+void Display_HashTable(int arr[], int chain[])
 {
-    for(int i=0;i<10;i++)
+    for (int i = 0; i < 10; i++)
     {
-        if(arr[i]!=-1)
+        if (arr[i] != -1)
         {
-           printf("[%d]  %d->%d\n",i,arr[i],chain[i]);
+            printf("[%d] %d -> %d\n", i, arr[i], chain[i]);
         }
-
     }
 }
 
-// 11 101 88 18 48 3 70 19 16 26
+
+ //11 101 88 18 70 3 50 19 16 26
